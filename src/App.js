4ef5dfd60a072764fbfe619b55b880eac544a78f
DESKTOP-HB65II7\Justin <jroy@sociusmarketing.com>
2020-05-09 17:03:12 -0400

    Add react components from vanilla

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React from 'react';
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
 import './App.css';
+
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
     return (
-    <div>
+    <GlobalProvider>
         <Header />
         <div className="container">
             <Balance />
             <IncomeExpenses />
+            <TransactionList />
+            <AddTransaction />
         </div>
-    </div>
+    </GlobalProvider>
     );
 }
 
